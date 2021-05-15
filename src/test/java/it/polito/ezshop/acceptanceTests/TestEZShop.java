9c3f18cf52f65d81749ca32920b34d5b30a8761a
Marco Smorti s281554 <s281554@studenti.polito.it>
2021-05-15 12:01:48 +0200

    docs: added getter/setter WB test for PT and O classes

--- src/test/java/it/polito/ezshop/acceptanceTests/TestEZShop.java
+++ src/test/java/it/polito/ezshop/acceptanceTests/TestEZShop.java
@@ -1,6 +1,9 @@
 package it.polito.ezshop.acceptanceTests;
 
 import it.polito.ezshop.classes.*;
+import it.polito.ezshop.data.Order;
+import it.polito.ezshop.data.ProductType;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -15,6 +18,8 @@ public class TestEZShop {
 		
 /////////////////////////////////////// Marco S.
 		validateProductCodeTestCase();
+		getterAndSetterProductTypeTestCase();
+		getterAndSetterOrderTestCase();
 
 
 /////////////////////////////////////// Francesco
@@ -26,6 +31,69 @@ public class TestEZShop {
 		
 		assertTrue(db.deleteSaleTransaction(1));
 		assertFalse(db.deleteSaleTransaction(10));
+	}
+	
+	@Test
+	public void getterAndSetterProductTypeTestCase() {
+		ProductType pt = new ProductTypeClass(1, 0, "location", "test", "this is a test", "2222222", 3.22);
+		
+		pt.setId(5);
+		Integer id = pt.getId();
+		assert(id == 5);
+		
+		pt.setQuantity(2);
+		Integer quantity = pt.getQuantity();
+		assert(quantity == 2);
+		
+		pt.setLocation("testLoc");
+		String location = pt.getLocation();
+		assert(location == "testLoc");
+		
+		pt.setNote("note");
+		String note = pt.getNote();
+		assert(note == "note");
+		
+		pt.setProductDescription("desc");
+		String productDesc = pt.getProductDescription();
+		assert(productDesc == "desc");
+		
+		pt.setBarCode("03030");
+		String barCode = pt.getBarCode();
+		assert(barCode == "03030");
+		
+		pt.setPricePerUnit(1.10);
+		Double price = pt.getPricePerUnit();
+		assert(price == 1.10);
+	}
+	
+	@Test
+	public void getterAndSetterOrderTestCase() {
+		//Integer orderId, Integer balanceId, String productCode, Double pricePerUnit, Integer quantity, String status
+		Order o = new OrderClass(1,-1, "333", 2.01, 5, "ISSUED");
+		
+		o.setOrderId(5);
+		Integer id = o.getOrderId();
+		assert(id == 5);
+		
+		o.setBalanceId(4);
+		Integer bid = o.getBalanceId();
+		assert(bid == 4);
+		
+		o.setProductCode("20202");
+		String pc = o.getProductCode();
+		assert(pc == "20202");
+		
+		o.setPricePerUnit(2.01);
+		Double price = o.getPricePerUnit();
+		assert(price == 2.01);
+		
+		o.setQuantity(22);
+		Integer qty = o.getQuantity();
+		assert (qty == 22);
+		
+		o.setStatus("PAYED");
+		String status = o.getStatus();
+		assert (status == "PAYED");
 	}
 	
 	@Test
