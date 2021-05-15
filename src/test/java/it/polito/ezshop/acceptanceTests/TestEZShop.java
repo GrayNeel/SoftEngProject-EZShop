f9128896d74c0d73a2c0013cf5e9c4e2eb98b565
best303 <s246762@studenti.polito.it>
2021-05-15 11:47:01 +0200

    feat: unit tests

--- src/test/java/it/polito/ezshop/acceptanceTests/TestEZShop.java
+++ src/test/java/it/polito/ezshop/acceptanceTests/TestEZShop.java
@@ -19,11 +19,31 @@ public class TestEZShop {
 
 /////////////////////////////////////// Marco C.
 		
+		String productCode = "";
+		String wrongProductCode = "";
+		
 		assertNotNull(db.getClosedSaleTransactionById(1));
 		assertNull(db.getClosedSaleTransactionById(10));
 		
+		assertFalse(db.getProductEntriesByTransactionId(1).isEmpty());
+		assertTrue(db.getProductEntriesByTransactionId(10).isEmpty());
+		
 		assertTrue(db.deleteSaleTransaction(1));
 		assertFalse(db.deleteSaleTransaction(10));
+		
+//		assertEqual(1,db.startReturnTransaction(returnTransaction));
+		
+		assertTrue(db.deleteReturnTransaction(1));
+		assertFalse(db.deleteReturnTransaction(10));
+		
+		assertNotNull(db.getReturnTransactionById(1));
+		assertNull(db.getReturnTransactionById(10));
+		
+		assertNotEquals(0,db.getPricePerUnit(productCode),0.01);
+		assertEquals(0,db.getPricePerUnit(wrongProductCode),0.01);
+		
+		
+		
 	}
 
 }
